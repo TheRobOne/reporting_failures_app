924c87e23b4ac5b799d7e5f706d9c469408527b1
Maciej Rusek <maciej.rusek1@pl.ibm.com>
2018-04-16 19:16:56 +0200

    added post for new failure

--- client/src/components/NewFailure/NewFailure.js
+++ client/src/components/NewFailure/NewFailure.js
@@ -8,15 +8,55 @@ class NewFailure extends Component {
   constructor() {
     super();
     this.state = {
-      building: "A"
+      building: "A",
+      room: "",
+      email: "",
+      description: ""
     };
   }
 
-  onChangeHandler(event) {
+  changeBuilding(event) {
     this.setState({ building: event.target.value });
   }
 
-  
+  changeRoom(event) {
+    console.log(this.state.room)
+    this.setState({ room: event.target.value });
+  }
+
+  changeEmail(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  changeDescription(event) {
+    this.setState({ description: event.target.value });
+  }
+
+  addNewFailure() {
+    console.log(
+      `roomNumber: ` +
+        this.state.room +
+        `building: ` +
+        this.state.building +
+        `description: ` +
+        this.state.description +
+        `authorEmail: ` +
+        this.state.email
+    );
+    fetch("http://localhost:3000/failures", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        roomNumber: this.state.room,
+        building: this.state.building,
+        description: this.state.description,
+        authorEmail: this.state.email
+      })
+    });
+  }
 
   render() {
     return (
@@ -25,14 +65,18 @@ class NewFailure extends Component {
           <div className="col">
             <label> Budynek: </label>
           </div>
-          <Building onChangeHandler={event => this.onChangeHandler(event)} />
+          <Building onChangeHandler={event => this.changeBuilding(event)} />
         </div>
         <br />
         <div className="row">
           <div className="col">
             <label> Sala: </label>
           </div>
-          <Room building={this.state.building} />
+          <Room
+            building={this.state.building}
+            value={this.state.room}
+            onChangeHandler={event => this.changeRoom(event)}
+          />
         </div>
         <br />
         <div className="row">
@@ -40,7 +84,13 @@ class NewFailure extends Component {
             <label> Adres Email: </label>
           </div>
           <div className="col">
-            <input className="form-control" type="text" id="email" />
+            <input
+              className="form-control"
+              type="text"
+              id="email"
+              value={this.state.email}
+              onChange={event => this.changeEmail(event)}
+            />
           </div>
         </div>
         <br />
@@ -54,11 +104,17 @@ class NewFailure extends Component {
               rows="4"
               cols="50"
               id="description"
+              value={this.state.description}
+              onChange={event => this.changeDescription(event)}
             />
           </div>
         </div>
         <br />
-        <button className="btn btn-primary" type="submit">
+        <button
+          className="btn btn-primary"
+          type="submit"
+          onClick={() => this.addNewFailure()}
+        >
           Dodaj
         </button>
       </form>
