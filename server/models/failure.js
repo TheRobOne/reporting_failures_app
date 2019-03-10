8a398b1441af145bb1a0275d7cebf1dadb305664
TheRobOne <m_rusek@vp.pl>
2019-03-10 21:20:24 +0100

    Add title to model on server side

--- server/models/failure.js
+++ server/models/failure.js
@@ -9,6 +9,10 @@ const fauilureSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    title: {
+        type: String,
+        required: true
+    },
     description: {
         type: String,
         required: true
